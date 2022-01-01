7d20ad870c2bbc0f2c0acd3cd782905d2cd79b64
Piotr Radlak <p10trek@o2.pl>
2022-01-01 10:56:10 +0100

    after first project checkpoint

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "twitterclone195a72b2": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    },
+    "api": {
+        "twitterclone": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "TwitterClone": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
